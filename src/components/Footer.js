9a296c4bf7118b1ffecfeff1f63c50fa1a6fb930
nmd102 <nmduncan@madison.k12.wi.us>
2024-03-24 15:43:09 -0500

    Why did these not commit???

---
+++ src/components/Footer.js
@@ -0,0 +1,16 @@
+import Link from "next/link";
+
+export default function Footer() {
+    return (
+        <footer>
+            <Link href="/" className="logo">
+                <h2>[W<span className="oring">est High</span><span className="close"> Sci Oly]</span></h2>
+            </Link>
+            <a href="mailto:kaventurini@madison.k12.wi.us" className="signature" aria-label="no click">
+                <p>kaventurini@madison.k12.wi.us</p>
+            </a>
+
+            <p>(617)-710-9501</p>
+        </footer>
+    )
+}
---
+++ src/components/Navbar.js
@@ -0,0 +1,43 @@
+import Link from 'next/link'
+
+import { gsap } from "gsap/dist/gsap";
+import { ScrollTrigger } from "gsap/dist/ScrollTrigger";
+
+gsap.registerPlugin(ScrollTrigger);
+
+import React, { useEffect, useRef } from "react";
+
+export default function Navbar() {
+    const nav = useRef();
+    useEffect(() => {
+        gsap.to(nav.current, {
+            y: "8vh",
+            scrollTrigger: { //error here
+                trigger: nav.current,
+                scrub: true,
+                start: "top top",
+                end: "bottom+=100 top",
+            }
+        })
+    }, []);
+    return (
+        <nav ref={nav}>
+            <Link href="/" className="logo">
+                <h2>[West<span className="oring"> Sci O</span><span className="close">]</span></h2>
+            </Link>
+            <div className="navLinks">
+                <Link href="/blog">
+                    <span>blog/news</span>
+                </Link>
+                <Link href="/photo-gallery">
+                    <span>photo gallery</span>
+                </Link>
+
+                <Link href="/sponsors">
+                    <span>sponsor us</span>
+                </Link>
+
+            </div>
+        </nav>
+    )
+}
