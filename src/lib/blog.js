b15b1b600ed033bb4294b0eed29a842c8283762c
nmd102 <nmduncan@madison.k12.wi.us>
2024-03-27 17:33:50 -0500

    More image fixes

--- src/lib/blog.js
+++ src/lib/blog.js
@@ -2,13 +2,13 @@ export const items = [
   {
     title: "Madison west gets into MIT",
     description: "Madison West High School gets accepted to the MIT tournament! Read more",
-    img: "/media/scroll.svg",
+    img: "./media/scroll.svg",
     slug: "mit-accepted"
   },
   {
     title: "What's next?",
     description: "What happens next? Maybe this website will have actual functionaly, contents, and oh no the website went down l + ratio\nhey its back up",
-    img: "/media/gsap.svg",
+    img: "./media/gsap.svg",
     slug: "whats-next"
   }
 ]
