5734fa3f7f6302d4e0a422ed7e8efa6ded2eba79
nmd102 <nmduncan@madison.k12.wi.us>
2024-04-21 15:52:24 -0500

    fixed a photo issue

--- src/lib/photo-gallery.js
+++ src/lib/photo-gallery.js
@@ -2,7 +2,7 @@ let PATH = "media/photo_gallery/";
 export const items = [
   {
     title: "kevin bao 2023",
-    icon: PATH + "kevin.JPG",
+    icon: PATH + "/kevin-bao-2023/kevin.JPG",
     description: "kevin for president",
     link: "/photo-gallery/kevin-bao-2023",
     width: 500,
