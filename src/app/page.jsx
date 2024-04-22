19bb49e5ef7b3db09a28b5d45139c37056aa5e20
nmd102 <nmduncan@madison.k12.wi.us>
2024-04-22 10:04:03 -0500

    Added opengraph and twitter images (hopefully)

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -3,8 +3,7 @@ import Home from "@/components/home";
 export const metadata = {
     title: "West Science Olympiad",
     description: "Official Site for West High Science Olympiad, a Science Olympiad team based in Madison, Wisconsin.",
-    "og:image": "https://i.imgur.com/fZOVTFb.jpeg"
 }
 export default function Page() {
-    return <Home />
+    return <Home/>
 }
--- src/lib/blog.js
+++ src/lib/blog.js
@@ -5,11 +5,6 @@ export const items = [
     slug: "mit-accepted"
   },
   {
-    title: "What's next?",
-    description: "What happens next? Maybe this website will have actual functionality, contents, and the people who maintain it might even fix the broken css",
-    slug: "whats-next"
-  },
-  {
     title: "This website is now live",
     description: "This website actually got hosted!",
     slug: "website-live"
