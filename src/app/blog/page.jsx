afe88141aa86b7879d57742a6548a36550d82f24
nmd102 <nmduncan@madison.k12.wi.us>
2024-05-15 16:08:46 -0500

    More css fixes

--- src/app/blog/page.jsx
+++ src/app/blog/page.jsx
@@ -14,7 +14,7 @@ export default function Blog() {
         </header>
         <main className={`${styles.main} ${styles.lessons}`}>
             {items.map((item, key) => {
-                return (<Link key={key} href={`blog/${item.slug}`} className={"blogLink"}>
+                return (<Link key={key} href={`blog/${item.slug}`} className={styles.blogLink}>
                     <button className={styles.lessonCard}>
                         <h2>{item.title}</h2>
                         <p>{item.description}</p>
